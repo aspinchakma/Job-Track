51d1fb3d4a91158a0ce622076342099528d21858
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-18 17:20:11 +0600

    primary setup

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <h3 className="text-9xl font-bold">
+      Moanoghar Residential School And College
+    </h3>
+  </StrictMode>
+);
