510af48eaec1266f14dd2e09b9bab6c6d669db5e
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-21 12:08:36 +0600

    on Auth state change added

--- src/Context/AuthProfider.jsx
+++ src/Context/AuthProfider.jsx
@@ -1,11 +1,14 @@
 import {
   createUserWithEmailAndPassword,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
 } from "firebase/auth";
+import { useEffect, useState } from "react";
 import { auth } from "../firebase/firebase.init";
 import { AuthContext } from "./MixContext";
 
 const AuthProfider = ({ children }) => {
+  const [user, setUser] = useState(null);
   // register
   const register = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
@@ -15,6 +18,18 @@ const AuthProfider = ({ children }) => {
   const signIn = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
+
+  // checking stage
+  useEffect(() => {
+    const unSubscribed = onAuthStateChanged(auth, (userInfo) => {
+      if (userInfo) {
+        setUser(userInfo);
+      }
+    });
+
+    return () => unSubscribed();
+  }, []);
+  console.log(user);
   const data = { register, signIn };
   return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
 };
